0eafaf73c1133d98c0e1dc161bd4d76cd221ce08
LDF <ldf@LDFdeMacBook-Pro.local>
2019-11-08 18:13:59 +0800

    basic construction

--- src/index.js
+++ src/index.js
@@ -6,10 +6,15 @@ import pager from './components/pager'
 import swiper from './components/swiper'
 import swiperItem from './components/swiper-item'
 
-let components = [
-	img, modal, pager, swiper, swiperItem
-]
 
+export { // demo用
+	img, 
+	modal, pager, swiper, swiperItem
+}
+
+let components = [ // 默认集成组件 其余组件按需引入
+	modal, pager, swiper, swiperItem
+]
 import './style.styl'
 const install = (Vue, options) => {
 	if (install.installed) return
@@ -18,10 +23,5 @@ const install = (Vue, options) => {
 }
 
 if (typeof window !== 'undefined' && window.Vue) install(window.Vue)
-
-// demo用
-export {
-	img, modal, pager, swiper, swiperItem
-}
 
 export default { install, version }
